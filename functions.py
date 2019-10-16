0fe093fec53db9c563a514941dd2321700ca1e80
Ubuntu <ubuntu@ip-172-31-23-218.ec2.internal>
2019-10-16 14:19:28 +0000

    fixed my divide chat bot condition and added a unit test

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 def get_chatbot_response(message):
     if message[:2] != "!!":
         return ''
-
     points, command, args = message.split(' ', 2)
     if command == "Hey":
         return "What's up!"
@@ -10,7 +9,7 @@ def get_chatbot_response(message):
         return int(num1) + int(num2)
     elif command == "divide":
         num1, num2 = args.split()
-        return num1 / num2
+        return int(num1) / int(num2)
     elif command == "say":
         return args
     else:
